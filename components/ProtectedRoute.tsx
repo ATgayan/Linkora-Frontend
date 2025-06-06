e6c7c075c251063b15f586e8d4887178d37606ba
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2025-06-06 23:41:50 +0530

    protect the routes

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -10,7 +10,7 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
 
   useEffect(() => {
     if (!loading && !user) {
-      router.push("/login"); // Redirect if not authenticated
+      router.push("/auth"); // Redirect if not authenticated
     }
   }, [user, loading, router]);
 
