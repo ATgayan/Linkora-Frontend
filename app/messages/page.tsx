206fba41879d6ad7bf001c0b9ee47b0871f2a28c
AThusithaGayan <thusithag@aplicy.com>
2025-08-03 13:55:43 +0530

    fix err

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,63 +1,24 @@
-// 'use client'
+'use client'
 
-// import { useEffect, useState } from 'react'
-// import {
-//   Chat,
-//   Channel,
-//   MessageList,
-//   MessageInput,
-//   Window,
-//   ChannelList
-// } from 'stream-chat-react'
-// import 'stream-chat-react/dist/css/v2/index.css'
-
-
-// import { useAuth } from '@/lib/useAuth'
+import { useEffect, useState } from 'react'
+import { useAuth } from '@/lib/useAuth'
 
 
 
-// export default function ChatPage() {
-//   const [channel, setChannel] = useState<any>(null)
-//   const [ready, setReady] = useState(false)
+export default function ChatPage() {
+  const [channel, setChannel] = useState<any>(null)
+  const [ready, setReady] = useState(false)
   
-//   const {user } =useAuth();
+  const {user } =useAuth();
 
-// //   useEffect(() => {
-// //     async function init() {
-// //       console.log('user',user);
-// //       if (!user) return
-// //       const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/chat/token`, {
-// //         userId: user?.uid || null,
-// //       })
 
-// //       await streamClient.connectUser(user as any, res.data.token)
 
-// //       const channel = streamClient.channel('messaging', {
-// //  members: [user?.uid || '', 'otherUserId'], // Replace 'otherUserId' with the actual ID of the user you want to chat with
-// //  })
+  if (!ready || !channel) return <div>Loading chat...</div>
 
-// //       await channel.watch()
-// //       setChannel(channel)
-// //       setReady(true)
-// //     }
+  return (
+    <div>
+      <h1>Chat Page</h1>
 
-// //     init()
-
-// //     return () => {
-// //       streamClient.disconnectUser()
-// //     }
-// //   }, [])
-
-//   if (!ready || !channel) return <div>Loading chat...</div>
-
-//   return (
-//     <Chat client={streamClient} theme='str-chat__theme-light'>
-//       <Channel channel={channel}>
-//         <Window>
-//           <MessageList />
-//           <MessageInput />
-//         </Window>
-//       </Channel>
-//     </Chat>
-//   )
-// }
+    </div>
+  )
+}
